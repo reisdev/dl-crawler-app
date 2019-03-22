ca7ed5febb51a2c03f00045488daaa10c47860e7
mthrsj <matheusdrdj@gmail.com>
2019-03-21 23:12:56 -0300

    Fixing catch error

--- src/index.js
+++ src/index.js
@@ -14,8 +14,6 @@ const init = async () => {
     .build();
 }
 
-init()
-
 const app = express()
 
 app.get("/", async (req, res) => {
@@ -27,12 +25,14 @@ app.get("/", async (req, res) => {
         return
     }
     try {
+        init()
         await driver.get("https://www.detran.mg.gov.br/habilitacao/cnh-e-permissao-para-dirigir/acompanhar-entrega-cnh")
         driver.wait(until.titleIs("DETRAN - MG - Acompanhar Entrega de CNH"), 2000);
         await driver.findElement(By.xpath(`//*[@class="cpf"]`)).sendKeys(CPF)
         await driver.findElement(By.xpath(`//*[@class="data obrigatorio"]`)).sendKeys(birthday)
         await driver.findElement(By.xpath(`//*[@class="uma_coluna"]/button`)).click()
         const msg = await driver.findElement(By.id("flashMessage")).getText()
+        driver.quit()
         res.status(404).send({message: "Erro: CNH não disponível. Entre em contato com o DETRAN"})
     } catch (error) {
         if (error.name == "NoSuchElementError") {
